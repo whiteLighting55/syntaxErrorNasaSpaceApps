e2e97cf0013da793c39a51c9dc5b0888b1f9fb93
Manu Franco <manuel_game555@hotmail.com>
2023-10-07 18:21:00 -0600

    .

---
+++ process_login.php
@@ -0,0 +1,25 @@
+<?php
+    $conectar = mysql_conect('localhost','root','');
+    if (!$conectar){
+        echo"no se pudo conectar a la base de datos";
+    }else{
+        $base = mysql_select_db("AstroXplore")
+        if (!$base){
+            echo "no se encuentra la base   "
+        }
+    }
+
+
+$ussername = $_POST['ussername'];
+$correo = $_POST['correo'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO ussers VALUES('$ussername','$correo','$password')"
+
+$ejecutar = mysql_querry($sql);
+if(!$ejecutar){
+    echo "hubo algun error";
+}else{
+    echo "datos guardados correctamente<br><a href = >volver</a>";
+}
+?>
